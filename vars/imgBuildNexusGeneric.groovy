b01e5e4e170bade8c84bf842a249fc04857aaed4
Logan Donley <ldonley@cloudbees.com>
2020-06-19 10:03:43 -0400

    Adding generic img push

---
+++ vars/imgBuildNexusGeneric.groovy
@@ -0,0 +1,18 @@
+// vars/imgBuildNexusGeneric.groovy
+def call(String imageName, String repoOwner, String registry, String dockerFile="Dockerfile", String imageTag = env.BUILD_NUMBER, Closure body) {
+  def label = "img-${UUID.randomUUID().toString()}"
+  def podYaml = libraryResource 'podtemplates/imageBuildPushNexus.yml'
+  podTemplate(name: 'img', label: label, yaml: podYaml) {
+    node(label) {
+      body()
+      imageNameTag()
+      gitShortCommit()
+      container('img') {
+        sh """
+          img build --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor="${env.COMMIT_AUTHOR}" -t ${registry}/${repoOwner}/${imageName}:${env.VERSION.trim()} ${pwd()/${dockerFile}}
+          img push ${registry}/${repoOwner}/${imageName}:${env.imageTag}
+        """
+      }
+    }
+  }
+}

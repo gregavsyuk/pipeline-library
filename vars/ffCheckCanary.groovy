ec23b2f5055803dce962482fd1e2828f9b32e559
Grigory Avsyuk <gavsyuk@Grigorys-MacBook-Pro.local>
2020-07-26 22:49:27 -0400

    Attempt to move from FF pipeline to shared library

--- vars/ffCheckCanary.groovy
+++ vars/ffCheckCanary.groovy
@@ -1,1 +1,15 @@
-
+def call(body) {
+	changedFile = sh (script:"git diff-tree --no-commit-id --name-only -r ${env.GIT_COMMIT}", returnStdout: true)
+	echo(changedFile)
+	experimentFile = changedFile.contains("experiments/")
+	if(experimentFile){
+		experimentText = sh (script:"cat ${changedFile}", returnStdout: true)
+		sh "cat ${changedFile}"
+	    experimentYaml = readYaml (text: "$experimentText")
+	    flagName = experimentYaml.flag
+	    if(experimentYaml.labels)
+	    {
+	      canaryBool = experimentYaml.labels.contains("Canary-deploy")
+	      return canaryBool
+	    }
+}

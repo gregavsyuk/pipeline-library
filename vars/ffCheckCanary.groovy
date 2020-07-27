87333e2ab7c89fc421f0561bc2bcb87f477989d0
Grigory Avsyuk <gavsyuk@Grigorys-MacBook-Pro.local>
2020-07-26 23:00:22 -0400

    Try using global yaml

--- vars/ffCheckCanary.groovy
+++ vars/ffCheckCanary.groovy
@@ -5,11 +5,12 @@ def call(body) {
 	if(experimentFile){
 		experimentText = sh (script:"cat ${changedFile}", returnStdout: true)
 		sh "cat ${changedFile}"
-	    experimentYaml = readYaml (text: "$experimentText")
-	    flagName = experimentYaml.flag
-	    if(experimentYaml.labels)
+	    env.experimentYaml = readYaml (text: "$experimentText")
+	    flagName = env.experimentYaml.flag
+	    if(env.experimentYaml.labels)
 	    {
-	      canaryBool = experimentYaml.labels.contains("Canary-deploy")
+	      canaryBool = env.experimentYaml.labels.contains("Canary-deploy")
 	      return canaryBool
 	    }
+	}
 }
